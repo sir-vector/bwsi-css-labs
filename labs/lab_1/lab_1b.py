38c02609a22454b053f5d900a2d731ff68cba59f
sir_vector <139803240+sir-vector@users.noreply.github.com>
2026-03-11 23:18:37 -0700

    Update lab_1b.py

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -37,13 +37,23 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
+def valid_number(prompt: str) -> float:
+    valid = False
+    while valid == False:
+        try:
+            number = float(input(prompt))
+            valid = True  # only reaches here if float() worked
+        except ValueError:
+            print("That's not a number, try again!")
+    return number
+
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = valid_number("Enter the first number: ")
+    num2 = valid_number("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
